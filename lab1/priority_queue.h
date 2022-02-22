d504b8ed19381b57c64d9bd460e8946490b42f0d
Matyash Andrei <ratati12>
2022-02-22 16:27:25 +0300

    Funcs -> class

--- lab1/priority_queue.h
+++ lab1/priority_queue.h
@@ -1,61 +1,64 @@
 using namespace std;
-void swap(int *a, int *b);
-void add(vector<int> &heapTree, int element);
-void sort_heapTree(vector<int> &heapTree, int i);
-void print_heapTree(vector<int> &heapTree);
-void check_empty(vector <int> &heapTree);
-
-void swap(int *a, int *b)
+class Priority_queue 
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void check_empty(vector<int> &heapTree)
-{
-    if (heapTree.size() == 0 ) cout << "The heap tree is empty\n";
-    else cout << "The heap tree contains " << heapTree.size() << " elements.\n";
-}
-
-void add(vector<int> &heapTree, int element)
-{
-    int i;
-    int size = heapTree.size();
-    if (size != 0) 
-    {
-        heapTree.push_back(element);
-        for (int i = size/2-1; i >= 0; i--)
+    public:
+        vector<int> heapTree;
+        void swap(int *a, int *b)
         {
-            sort_heapTree(heapTree, i);
+            int temp = *a;
+            *a = *b;
+            *b = temp;
         }
-    }
-    else
-    {
-        heapTree.push_back(element);
-    }
-}
 
-void sort_heapTree(vector<int> &heapTree, int i)
-{
-    int size = heapTree.size();
-    int max = i;
-    int left = 2*i+1;
-    int right = 2*i+2;
-    if ((left < size) && (heapTree[left] > heapTree[max])) max = left;      //looking for the max element 
-    if ((right < size) && (heapTree[right] > heapTree[max])) max = right;   //between left and right elements
-    if (max != i)
-    {
-        swap(&heapTree[i], &heapTree[max]);
-        sort_heapTree(heapTree, max);
-    }
-}
+        void check_empty()
+        {
+            if (heapTree.size() == 0 ) cout << "The heap tree is empty\n";
+            else cout << "The heap tree is not empty!\n"; 
+        }
 
-void print_heapTree(vector<int> &heapTree)
-{
-    for (int i = 0; i < heapTree.size(); i++) 
-    {
-        cout << heapTree[i] << "  ";
-    }
-    cout << "\n";
-}
+        void heapTree_size()
+        {
+            cout << "The heap tree contains " << heapTree.size() << " elements.\n";
+        }
+        void add(int element)
+        {
+            int i;
+            int size = heapTree.size();
+            if (size != 0) 
+            {
+                heapTree.push_back(element);
+                for (int i = size/2-1; i >= 0; i--)
+                {
+                    sort_heapTree(i);
+                }
+            }
+            else
+            {
+                heapTree.push_back(element);
+            }
+        }
+
+        void sort_heapTree(int i)
+        {
+            int size = heapTree.size();
+            int max = i;
+            int left = 2*i+1;
+            int right = 2*i+2;
+            if ((left < size) && (heapTree[left] > heapTree[max])) max = left;      //looking for the max element 
+            if ((right < size) && (heapTree[right] > heapTree[max])) max = right;   //between left and right elements
+            if (max != i)
+            {
+                swap(&heapTree[i], &heapTree[max]);
+                sort_heapTree(max);
+            }
+        }
+
+        void print_heapTree()
+        {
+            for (int i = 0; i < heapTree.size(); i++) 
+            {
+                cout << heapTree[i] << "  ";
+            }
+            cout << "\n";
+        }
+};
--- lab1/test_queue.cpp
+++ lab1/test_queue.cpp
@@ -6,21 +6,28 @@ using namespace std;
 /*
  * Functions priority_queue:
  * swap(int *a, int *b)
- * add(vector<int> &heapTree, int element)
- * void sort_heapTree(vector<int> &heapTree, int i);
- * void print_heapTree(vector<int> &heapTree);
- * void check_empty(vector <int> &heapTree);
-*/
+ * add(int element)
+ * void sort_heapTree(int i);
+ * void print_heapTree();
+ * void check_empty();
+ * void heapTree_size()
+ * 
+ * */
+
 int main()
 {
-    vector<int> heapTree;
+    Priority_queue tree;
 
-    check_empty(heapTree);
-    add(heapTree, 7);
-    add(heapTree, 1);
-    add(heapTree, 3);
-    add(heapTree, 9);
-    add(heapTree, 2);
-    add(heapTree, 4);
-    check_empty(heapTree);
+    tree.check_empty();
+    cout << "/* tree.add(9);\n   tree.add(3); \n   tree.add(5);\n   tree.add(1);\n   tree.add(4);\n   tree.add(2);\n   tree.add(7);*/\n";
+    tree.add(9);
+    tree.add(3);
+    tree.add(5);
+    tree.add(1);
+    tree.add(4);
+    tree.add(2);
+    tree.add(7);
+    tree.check_empty();
+    tree.heapTree_size(); 
+    tree.print_heapTree();
 }
